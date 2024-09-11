c45fea348ef56d6598ae06d6933c807ee562f0c4
Siyavush1411 <152807458+Siyavush1411@users.noreply.github.com>
2024-09-12 00:59:27 +0300

    Created first template and main page

---
+++ template/Header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Каталог</title>
+    <link rel="stylesheet" href="../assets/css/HeaderStyle.css"> 
+    <link rel="stylesheet" href="../assets/css/Style.css"> 
+</head>
+<body>
+    <header>
+        <div class="navbar">
+            <h1 class="logo-name">Каталог услуг</h1>
+            <nav>
+                <ul>
+                    <li><a href="index.php">Главная</a></li>
+                    <li><a href="add_card.php">Регистрация</a></li>
+                    <div class="callback">
+                        <li><a href="view_cards.php">Обратная связь</a></li>
+                    </div>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <main>
